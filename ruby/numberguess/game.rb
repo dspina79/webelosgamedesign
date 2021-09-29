411af89e10496c75602ab6081d241642ae48a2db
Dave Spina <dspina79@gmail.com>
2021-09-28 20:49:06 -0400

    Added guessing game

---
+++ ruby/numberguess/game.rb
@@ -0,0 +1,42 @@
+# Simple Pick a Number Game
+
+def generate_number(max)
+    return (rand() * max).to_i
+end
+
+def play_round()
+    print("What is your name? ")
+    name = gets.chomp
+    print("Hello #{name}. Welcome to the game!\n")
+    max_num = 50
+    actual_num = generate_number(max_num)
+    print("\nI am thinking of a number between 1 and #{max_num}. \nWhat is it?")
+    tries = 0
+    win = false
+    while tries < 8
+        if tries == 0
+            print("\nYour guess: ")
+        else 
+            print("\nTry again: ")
+        end
+        tries += 1
+        guess = gets.chomp.to_i
+
+        if guess == actual_num
+            print("You got it, #{name}!!!\nYOU ARE A WINNER!!!\n\n")
+            break
+        elsif guess < actual_num
+            print("Sorry, the number is higher than #{guess}.\n")
+        else
+            print("Sorry, the number is lower than #{guess}.\n")
+        end     
+    end
+    # assuming we are beyond 8 guesses
+    if !win
+        print("\nYou did not guess in the number of times required.")
+        print("\nThe real numbers was #{actual_num}")
+        print("\nGame Over\n\n\n")
+    end
+end
+
+play_round()
